fe2436fdaea366778d33dbb9ae74a81a0d5c3c99
Igor Amantino <justin.amantino@gmail.com>
2020-09-06 00:10:28 -0300

    Server-2Modulo

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4c54as5w45svbnmbne645s4a4w8s5wa',
+    expiresIn: '1d',
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
-import express from 'express';
-import routes from './routes';
+import 'reflect-metadata';
 
-const app=express();
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './Errors/AppError';
+
+import './database';
+
+const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.listen (3333,()=>{
-    console.log('Server Start ');
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.log(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server Error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('Server Start 🔛🔛  ');
 });
